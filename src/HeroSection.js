6b65ecde99d28d9afdb8ec992b297f540a87fe60
AishwaryaRajput09 <aishwaryanaglot9144@gmail.com>
2022-12-24 22:34:11 +0530

    sections added

---
+++ src/HeroSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HeroSection = () =>{
+    return(
+        <div className="jumbotron">
+            <h1 className="display-4">
+                App looks cool!
+            </h1>
+            <p className="lead">
+                Lorem jjhcf ededg edeiu8ef 09wy gcydh edcguhd cfihsug d fcidhuv fcisdbs fcisdbcsd isdhcsu
+            </p>
+            <hr className="my-4"/>
+                <p>
+                Lorem jjhcf ededg edeiu8ef 09wy gcydh edcguhd cfihsug d fcidhuv fcisdbs fcisdbcsd isdhcsu
+                </p>
+            <a className="btn btn-success btn-lg" href="#" role="button">React is awesome</a>
+        </div>
+    );
+}
+export default HeroSection;
---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const footer = () =>{
+    return(
+        <section className="footer bg-dark text-white">
+            <p className="lead container">Contact Us on instagram</p>
+            <p className="lead container">Reach out through email</p>
+            <p className="lead container">No Copyright</p>
+        </section>
+    );
+}
+export default footer;
